502bc8d122a1a384ff59677f865a87cf108aeec9
An Phan <phanhoaian0608@gmail.com>
2025-06-17 14:43:51 +0700

    initial commit

---
+++ src/test/java/com/example/TradingProject/TradingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TradingProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
